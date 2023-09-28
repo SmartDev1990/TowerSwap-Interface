146767dd9986bf582b7ca0539ec70c781654b7c3
Smartdev <95247835+SmartDev1990@users.noreply.github.com>
2023-09-28 22:14:28 +0700

    Update bandit sdk

--- apps/web/src/views/Nft/market/Home/index.tsx
+++ apps/web/src/views/Nft/market/Home/index.tsx
@@ -19,14 +19,16 @@ declare global {
 
 const Home: React.FC = () => {
   const { t } = useTranslation()
-  useEffect(() => {
-    const script = document.createElement('script')
-    script.src = 'https://sdk.bandit.network/static/js/index.js'
-    script.async = true
+  
+useEffect(()=>{
+    const script = document.createElement("script")
+    script.src = "https://sdk.bandit.network/sdk/index.js"
+    script.type = "module"
     script.onload = () => window.renderStats()
-
+    
     document.body.appendChild(script)
-  }, [])
+},[])
+  
 
   return (
     <>
