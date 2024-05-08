32bcfe80d7cc1af2cfd82233f6dce2b90a1857ce
Smartdev <95247835+SmartDev1990@users.noreply.github.com>
2024-05-09 05:13:00 +0700

    add Zeta

--- apps/web/src/utils/wagmi.ts
+++ apps/web/src/utils/wagmi.ts
@@ -10,7 +10,7 @@ import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { SafeConnector } from '@gnosis.pm/safe-apps-wagmi'
 
-const CHAINS = [base, shardeumValidators]
+const CHAINS = [zetaMainnet, base, shardeumValidators]
 
 const getNodeRealUrl = (networkName: string) => {
   let host = null
