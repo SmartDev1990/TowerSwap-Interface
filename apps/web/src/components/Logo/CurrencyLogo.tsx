a40304c062f2a7f7b66758fbfb60b113ec91ccfd
towerdevswap <akmalhanif672@gmail.com>
2024-05-24 00:58:43 +0700

    fix token0

--- apps/web/src/components/Logo/CurrencyLogo.tsx
+++ apps/web/src/components/Logo/CurrencyLogo.tsx
@@ -41,7 +41,7 @@ export default function CurrencyLogo({
   }, [currency, uriLocations])
 
   if (currency?.isNative) {
-    if (currency.chainId === ChainId.BASE) {
+    if (currency.chainId === ChainId.ZETA) {
       return <BinanceIcon width={size} style={style} />
     }
     return <StyledLogo size={size} srcs={[`/images/chains/${currency.chainId}.png`]} width={size} style={style} />
